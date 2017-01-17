a035fd0f3c7985bb5bb3072540bace83da5e40af
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-01-17 13:16:31 +0100

    projects: Add "Improve Diff Handling"

--- data/projects.js
+++ data/projects.js
@@ -1,10 +1,38 @@
 projects = [
 	{
-		"name" : "Project 1",
-		"desc" : "Desc",
-		"mentors" : [
-		"user1",
-		"user2"
+		"name" : "Improve Diff Handling",
+		"desc" : "Bears should be able to offer more than just one possible patch \
+for an issue. Users should be able to choose between different patches, e.g. \
+for different possible spelling corrections.\
+\
+In addition bears should be able to offer templated patches, e.g. to offer \
+variable renamings while the user should be asked for the new variable name.",
+		"requirements" : [
+			"good concept in the proposal",
+			"familiarity with the coala core"
+		],
+		"difficulty" : "low",
+		"issues" : [
+			"https://github.com/coala/coala/issues/1021"
 		]
+		"mentors" : [
+			"sils",
+			"jayvdb"
+		],
+		"milestones" : {
+			"GSoC 2017 Coding Starts" : [
+				"A cEP for the project has been merged."
+			],
+			"GSoC 2017 Midterm" : [
+				"The user is able to choose between multiple patches.",
+				"At least one bear exists that makes use of this."
+			],
+			"GSoC 2017 Final" : [
+				"The user is able to apply templated patches.",
+				"At least one bear exists that makes use of this."
+			]
+		},
+		"initiatives" : ["GSoC"],
+		"tags" : ["Core", "CLI"]
 	}
 ]
