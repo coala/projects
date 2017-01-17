ec3904020b727d19edec8b090fcaa7031b4f493b
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-01-17 14:02:54 +0100

    faq: Rework great proposal answer

--- data/faq.js
+++ data/faq.js
@@ -7,12 +7,12 @@ most importantly the projects page."
 {
 	"ques": "How do I work out a great proposal?",
 	"ans" : "Please design your project in a way that you have **multiple \
-checkpoints**. We'd like you to specify at least three partial goals so if \
-something goes wrong for a valid reason, we can rearrange your schedule \
-meaningfully. Each of those goals has to be merged into the main coala \
-codebase in order for it to be considered \"complete\". You should be aware \
-of what that involves for you (writing tests, going through extensive code \
-review, learning :), reiterating).\
+checkpoints**. We'd like you to specify at least three milestones with \
+specific goals: if something goes wrong for a valid reason, we can rearrange \
+your schedule meaningfully. Each of those goals has to be merged into the main \
+coala codebase in order for it to be considered \"complete\". You should be \
+aware of what that involves for you (writing tests, writing docs, going \
+through extensive code review, learning :), reiterating).\
 \
 You can use any project idea listed here or one that came out of your mind - \
 however be sure to make us aware of your plans and start planning your project \
