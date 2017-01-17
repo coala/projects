59572d5c98ca90b606e27f486cc2772002dfee68
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-01-17 13:54:04 +0100

    faq: Add meaningful questions
    Imported from the wiki.

--- data/faq.js
+++ data/faq.js
@@ -1,14 +1,26 @@
 faq = [
 {
-	"ques": "Question 1",
-	"ans" : "Answer"
+	"ques": "I want to do a GSoC!",
+	"ans" : "Great! This is the right place. Check out the other questions and \
+most importantly the projects page."
 },
 {
-	"ques": "Question 2",
-	"ans" : "Answer"
-},
-{
-	"ques": "Question 3",
-	"ans" : "Answer"
+	"ques": "How do I work out a great proposal?",
+	"ans" : "Please design your project in a way that you have **multiple \
+checkpoints**. We'd like you to specify at least three partial goals so if \
+something goes wrong for a valid reason, we can rearrange your schedule \
+meaningfully. Each of those goals has to be merged into the main coala \
+codebase in order for it to be considered \"complete\". You should be aware \
+of what that involves for you (writing tests, going through extensive code \
+review, learning :), reiterating).\
+\
+You can use any project idea listed here or one that came out of your mind - \
+however be sure to make us aware of your plans and start planning your project \
+with us so we can coordinate and - in the worst case - prohibit waste of time \
+from your side. At all cost make sure you find something that interests you. \
+We want this to be a fun and educating experience for you!\
+\
+Use the \"draft\" feature on the GSoC system, submit your draft early, get a \
+lot of helpful feedback from us!"
 }
 ]
