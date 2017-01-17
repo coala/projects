1ce346283d547f998691fb776df152d038c75f89
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-01-17 16:15:59 +0100

    projects: Add metrics project

--- data/projects.js
+++ data/projects.js
@@ -133,5 +133,53 @@ framework, if needed as a cEP."
 		},
 		"initiatives" : ["GSoC"],
 		"tags" : ["Linter", "Bears"]
+	},
+	{
+		"name" : "Implement Metrics for coala",
+		"desc" : "There is more to software quality than just passing builds. A \
+wide variety of design principles influence software quality, and some of \
+them can be measured objectively by so-called \
+[software metrics](https://en.wikipedia.org/wiki/Software_metric). coala \
+offers a good base for the implementation or wrapping of algorithms that \
+calculate these metrics from a piece of code.\
+\
+The goal of this project proposal should be to implement handling and \
+display of software metrics in coala. To prove the concept, this project \
+has to include multiple bears that calculate metrics for different \
+programming languages.",
+		"requirements" : [
+			"At least one patch to the coala core should be accepted and merged.",
+			"The student should be familiar with ongoing technical cEPs.",
+			"The proposal needs to contain a clear concept."
+		],
+		"difficulty" : "high",
+		"issues" : [
+			"https://github.com/coala/coala/issues/759"
+		],
+		"mentors" : [
+			"fneu",
+			"jayvdb",
+			"sils"
+		],
+		"milestones" : {
+			"GSoC 2017 Coding Starts" : [
+				"The applicant has created CLI and web UI mockups for metrics.",
+				"The applicant has created a cEP for the planned changes."
+			],
+			"GSoC 2017 Midterm" : [
+				"The API for bears to yield metrics has been implemented.",
+				"The API is properly documented and fully tested.",
+				"At least one Bear implementing the API exists.",
+				"A working (not clean or merged!) prototype of the CLI visualization \
+exists."
+			],
+			"GSoC 2017 Final" : [
+				"The coala CLI can visualize metrics.",
+				"The other coala binaries can handle metrics.",
+				"User documentation is written."
+			]
+		},
+		"initiatives" : ["GSoC"],
+		"tags" : ["Core", "Bears"]
 	}
 ]
