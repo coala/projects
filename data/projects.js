71c345f3a37a2d837823ab2cdfebe3c26fd95cfc
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-01-17 15:01:44 +0100

    projects: Add jenkins project

--- data/projects.js
+++ data/projects.js
@@ -36,5 +36,48 @@ variable renamings while the user should be asked for the new variable name.",
 		},
 		"initiatives" : ["GSoC"],
 		"tags" : ["Core", "CLI"]
+	},
+	{
+		"name" : "Create Jenkins Plugin",
+		"desc" : "Jenkins has some neat way to show code analysis results - let's \
+use that to provide Jenkins users an awesome usability! The result of this \
+project should be a full fledged Jenkins integration, living in an own \
+repository with a full testsuite.\
+\
+If you find this fun you'll be the one knowing this component best and will \
+get the chance to maintain it on your own if you want - always with the \
+support of the community guarding your back of course.",
+		"requirements" : [
+			"Any patch to jenkins has been accepted.",
+			"The applicant is familiar with the coala plugins.",
+			"Any patch to any coala plugin has been accepted and merged."
+		],
+		"difficulty" : "low",
+		"issues" : [
+			"https://github.com/coala/coala/issues/2757"
+		],
+		"mentors" : [
+			"fneu",
+			"Udayan12167",
+			"jayvdb"
+		],
+		"milestones" : {
+			"GSoC 2017 Coding Starts" : [
+				"A test jenkins instance exists.",
+				"An official repository exists with rultor set up."
+			],
+			"GSoC 2017 Midterm" : [
+				"A working plugin with minimal functionality exists.",
+				"A basic testsuite exists.",
+				"Basic documentation exists."
+			],
+			"GSoC 2017 Final" : [
+				"A working plugin with full functionality exists.",
+				"A full covering testsuite exists.",
+				"Full documentation exists."
+			]
+		},
+		"initiatives" : ["GSoC"],
+		"tags" : ["Plugins", "CI"]
 	}
 ]
