43ac60ce9b28efd33cad406a4db504957f5c8e5e
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-01-17 15:31:59 +0100

    projects: Add "Extend Linter Integration"

--- data/projects.js
+++ data/projects.js
@@ -79,5 +79,59 @@ support of the community guarding your back of course.",
 		},
 		"initiatives" : ["GSoC"],
 		"tags" : ["Plugins", "CI"]
+	},
+	{
+		"name" : "Extend Linter Integration",
+		"desc" : "A very easy way to cover a broad spectrum of analysis routines \
+is wrapping linters into coala. Currently we have a `Lint` class which \
+simplifies this integration. Based on this class, a large selection of \
+linters can be implemented rapidly. A tutorial on writing linter bears \
+is available at [the linter documentation]\
+(http://api.coala.io/en/latest/Developers/Writing_Linter_Bears.html).\
+The number may vary based on the difficulty to integrate any given tool.\
+\
+This project proposal should contain a concrete list of linters that are \
+wrapped together with concrete improvement proposals to the `Lint` class \
+if needed. Because implementing a linter bear is a rather trivial task it is \
+highly recommended to spice up the project with something interesting, \
+like adding project wide linter support to the Lint class. Improving \
+integrations by adding more options can also be part of this.\
+\
+Upstream contributions to linters are encouraged, e.g. to add auto fixing \
+abilities.",
+		"requirements" : [
+			"The applicant has added an arbitrary linter to coala.",
+			"The proposal contains a list of linters to integrate for every \
+milestone, split into two batches."
+		],
+		"difficulty" : "medium",
+		"issues" : [
+			"https://github.com/coala/engagement/issues/66"
+		],
+		"mentors" : [
+			"AbdealiJK",
+			"Makman2",
+			"fneu",
+			"jayvdb"
+		],
+		"milestones" : {
+			"GSoC 2017 Coding Starts" : [
+				"The applicant has tried out all linters manually.",
+				"The applicant has outlined all changes needed to the linter \
+framework, if needed as a cEP."
+			],
+			"GSoC 2017 Midterm" : [
+				"The first batch of linters have been merged.",
+				"At least one improvement to the linter framework is in review state.",
+				"All of them are properly tested and documented."
+			],
+			"GSoC 2017 Final" : [
+				"The second batch of linters have been merged.",
+				"At least one improvement to the linter framework has been merged.",
+				"All of them are properly tested and documented."
+			]
+		},
+		"initiatives" : ["GSoC"],
+		"tags" : ["Linter", "Bears"]
 	}
 ]
