1e13cd3663068a0efb26482160fc941d20d2ab4a
Fabian Neuschmidt <fabian@neuschmidt.de>
2017-01-18 21:27:29 +0100

    projects: Add 'Cohesive Editor Support' project

--- data/projects.js
+++ data/projects.js
@@ -181,5 +181,50 @@ exists."
 		},
 		"initiatives" : ["GSoC"],
 		"tags" : ["Core", "Bears"]
+	},
+	{
+		"name" : "Cohesive Editor Support",
+		"desc" : "Develop an efficient strategy that allows coala to maintain a number \
+of plugins for different editors. We consider any bit of code *supported* only if we have regular \
+continuous integration and tests running to ensure it's functionality. coala features quite some \
+editor plugins already, none of which are *supported* according to the above definition. The goal \
+of this project is to plan, introduce and configure a testing routine that allows coala to ensure \
+the functioning of it's editor plugins at all times. A viable approach might be to set up weekly \
+tests for the current editor plugins. For some editors like sublime and gedit, running tests is \
+not easy and there is no api to do it. In such cases we  would want to use dogtail (a GUI tester) \
+or a similar tool to do this.",
+		"requirements" : [
+			"The proposal has to provide a convincing strategy for testing editor plugins",
+			"The applicant should have one patch accepted to any of the coala projects."
+		],
+		"difficulty" : "medium",
+		"issues" : [
+			"https://github.com/coala/coala-atom/issues/37",
+			"https://github.com/coala/coala-emacs/issues/11",
+			"https://github.com/coala/coala-eclipse/issues/25",
+			"https://github.com/coala/coala-vim/issues/24",
+			"https://github.com/coala/coala-gedit/issues/5",
+			"https://github.com/coala/coala-sublime/issues/4"
+		],
+		"mentors" : [
+			"sils",
+			"AbdealiJK",
+			"fneu"
+		],
+		"milestones" : {
+			"GSoC 2017 Coding Starts" : [
+				"A cEP for the testing strategy has been merged."
+			],
+			"GSoC 2017 Midterm" : [
+				"The testing strategy has been set up for coala",
+				"At least one of coala's editor plugins is continually tested"
+			],
+			"GSoC 2017 Final" : [
+				"At least three of coala's editor plugins are continually tested",
+				"Documentation is available to easily set up testing for future editor plugins"
+			]
+		},
+		"initiatives" : ["GSoC"],
+		"tags" : ["Editors", "Plugins"]
 	}
 ]
