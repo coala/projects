f17a50040e5166453a6b996cc0cdea1ce50b0b47
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-01-19 17:48:09 +0100

    FAQ: Add mentor obligations

--- data/faq.js
+++ data/faq.js
@@ -55,5 +55,16 @@ The earlier you start, the better!"
 - Get stuff merged, before the deadlines!\
 - Get in contact with your mentors or the admins if any even remotely \
   potential problems arise."
+},
+{
+	"ques": "What are my obligations as a mentor during the GSoC?",
+	"ans" : "We expect you to:\
+\
+- Be active on [gitter](https://coala.io/chat).\
+- Blog at least once per milestone (this will be aggregated on \
+	[blog.coala.io](https://blog.coala.io)).\
+- Participate in weekly meetings with your student.\
+- Review your students pull requests timely.\
+- Get in contact with the admins if any even remotely potential problems arise."
 }
 ]
