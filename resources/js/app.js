228f37ed33b81ffac5c9a1f37b6c4df16126439d
Hemang Kumar <hemangsk@gmail.com>
2017-01-20 19:54:45 +0530

    app.js: Add url search for project
    Add search parameter whose value
    goes in the search box
    
    Fixes https://github.com/coala/projects/issues/96

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 (function(){
-	var app = angular.module('coala', ['ngSanitize','btford.markdown']);
+	var app = angular.module('coala', ['ngRoute','ngSanitize','btford.markdown'])
 
 	app.controller('TabController', function () {
 		this.tab = 1
@@ -11,24 +11,64 @@
 		}
 	})
 
-	app.directive('projects',  ['$http',  '$timeout' ,function ($http, $timeout) {
-		return {
-			restrict: 'E',
-			templateUrl: '/partials/tabs/projects.html',
-			controller: function ($scope) {
-				self = this
-				$scope.projectList = projects
+	app.directive('projects',  ['$http', '$timeout' ,'$location', 
+		function ($http, $timeout, $location) {
+			return {
+				restrict: 'E',
+				templateUrl: '/partials/tabs/projects.html',
+				controller: function ($scope) {
+					self = this
+
+					$scope.projectList = projects
+					$scope.projects_url_dict = {}
+
+					angular.forEach($scope.projectList, function(value, key){
+						value["url"] = value["name"].split(' ').join('_').toLowerCase();
+						$scope.projects_url_dict[value["url"]] = key
+					});
+
+				/* When project details view modal is closed
+				this function is called from modal callback 
+				from projects.html L100
+				*/
+				$scope.updateLink = function () {
+					$location.path('');
+				}
+
+				/* This sets the clicked project as
+				selected project, updates the url in
+				browser and opens the modal */
 				self.showProject = function (project) {
 					$(document).ready(function () {
-						$('#modal1').modal('open');
+						mval = project["name"].split(' ').join('_').toLowerCase();
+						$location.path('/' + mval)
+						$('.modal').modal('open');
 					});
 					$scope.currentProject = project
 				}
 
-			},
-			controllerAs: 'lc'
-		}
-	}]);
+				/* If the url has parameters, then this section is
+				executed. It checks whether url has a search query
+				or is it project url. */ 
+				var searchObject = $location.url();
+				if(searchObject){
+					searchObject = searchObject.split('=');
+					if(searchObject.length == 2 && searchObject[0].indexOf('search') ==1){
+						$scope.searchText = searchObject[1].replace(/%20/g, ' ')
+					}else if(searchObject.length == 1){
+						searchObject[0] = searchObject[0].substring(1, searchObject[0].length);
+
+						if(Object.keys($scope.projects_url_dict).indexOf(searchObject[0]) >= -1){
+							self.showProject($scope.projectList[$scope.projects_url_dict[searchObject[0]]])
+
+						}
+					}
+				}
+
+				},
+				controllerAs: 'lc'
+				}
+				}]);
 
 	app.directive('faq',[ '$http', function ($http) {
 		return {
@@ -37,27 +77,35 @@
 			controller: function () {
 				self = this
 				self.faqs = faq
-		},
-		controllerAs: 'toc'
-	}
+			},
+			controllerAs: 'toc'
+		}
 	}]);
 
 
 	app.filter('format_desc', function () {
-        return function (value) {
-            if (!value) return '';
-            var lastspace = value.indexOf('.');
-            if (lastspace != -1) {
-                if (value.charAt(lastspace-1) == ',') {
-                	lastspace = lastspace - 1;
-                }
-                  value = value.substr(0, lastspace);
-            }
+		return function (value) {
+			if (!value) return '';
+			var lastspace = value.indexOf('.');
+			if (lastspace != -1) {
+				if (value.charAt(lastspace-1) == ',') {
+					lastspace = lastspace - 1;
+				}
+				value = value.substr(0, lastspace);
+			}
 
-            return value;
-        };
-    });
+			return value;
+		};
+	});
 
+	app.filter('format_name', function () {
+		return function (value) {
+			if(value){
+				value  = value.split(' ').join('_').toLowerCase();
+			}	
+			return value;
+		};
+	});
 
 	app.directive('mentors', ['$http', function ($http) {
 		return {
@@ -70,8 +118,8 @@
 				angular.forEach(projects, function(value, key){
 					angular.forEach(value.mentors, function(value, key){
 						self.mentorsList[value] =  {
-									"github_handle" : value,
-									"github_avatar_url": "https://avatars.githubusercontent.com/" +value
+							"github_handle" : value,
+							"github_avatar_url": "https://avatars.githubusercontent.com/" +value
 						}
 
 					});
