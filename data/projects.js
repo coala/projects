8a2dd978b28d986932e6d74e13f4449909dfbca0
Adrianzatreanu <zgadi_2007@yahoo.com>
2017-01-21 15:25:54 +0200

    Add ``improve_installation`` as a project

--- data/projects.js
+++ data/projects.js
@@ -322,5 +322,26 @@ coala should support generating metrics for your code.",
 		"tags" : ["reStructuredText", "Sphinx"],
 		"markdown": "rstcheck_with_better_sphinx_support.md",
 		"collaborating_projects" : ["coala", "rstcheck"]
+	},
+	{
+		"name" : "Improve installation of coala and coala-bears",
+		"desc" : "This project is about improving the \
+		[bear installation tool](https://gitlab.com/coala/cib) and \
+		working around platform dependent requirements.",
+		"requirements" : [
+				"The participant should have one patch to [coala-bears](https://github.com/coala/coala-bears) accepted.",
+				"Huge plus is being familiar with the current design of cib",
+				"Knowledge of conda is a bonus, but not required."
+		],
+		"difficulty" : "medium",
+		"issues" : [
+			"https://github.com/coala/coala-bears/issues/845",
+			"https://github.com/coala/coala-bears/issues/664"
+		],
+		"mentors" : ["Adrianzatreanu", "Udayan12167", "fneu"],
+		"initiatives" : ["GSoC", "RGSoC"],
+		"tags" : ["Bears", "cib"],
+		"markdown": "improve_installation.md",
+		"collaborating_projects" : ["coala"]
 	}
 ]
