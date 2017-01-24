96f7f3dc37cc15293603048d5f7e3a57c2480b32
Daniel Teixeira <ddtxra@gmail.com>
2017-01-24 09:28:55 +0100

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -1,7 +1,7 @@
 projects = [
 	{
-		"name" : "Cellosaurus Enhancements",
-		"desc" : "Cellosaurus Enhancements ",
+		"name" : "Cellosaurus Web-Tool and Wikidata bot",
+		"desc" : "The students should develop a web tool to compare STR Marker profile for a cell line and a Wikidata bot",
 		"requirements" : [
 			"The proposal has to contain a clear concept.",
 			"The applicant should have one patch accepted to the coala core."
