8547928eeff4e7a92cc7c9ff2cb23fe3997f0820
Hemang Kumar <hemangsk@gmail.com>
2017-01-27 18:05:08 +0530

    app.js: Indent code

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,22 @@
 (function(){
 	var app = angular.module('coala', ['ngSanitize','btford.markdown', 'ngRoute']);
 
-		app.config(['$routeProvider',
-  	function($routeProvider) {
-	   $routeProvider.
-	   when('/', {
-	       redirectTo: '/projects'
-	   }).
-	   when('/projects', {
-	       template: '<projects></projects>'
-	   }).
-	   when('/mentors', {
-	       template: '<mentors></mentors>'
-	   }).
-	   when('/faq', {
-	       template: '<faq></faq>'
-	   })
- }]);
+	app.config(['$routeProvider',
+		function($routeProvider) {
+			$routeProvider.
+			when('/', {
+				redirectTo: '/projects'
+			}).
+			when('/projects', {
+				template: '<projects></projects>'
+			}).
+			when('/mentors', {
+				template: '<mentors></mentors>'
+			}).
+			when('/faq', {
+				template: '<faq></faq>'
+			})
+		}]);
 	app.controller('TabController', function ($location) {
 		this.tab = $location.path()
 		this.setTab = function (stab) {
@@ -40,43 +40,43 @@
 					$scope.currentProject = project
 
 					$(document).ready(function () {
-						 $('.modal').modal('open');
+						$('.modal').modal('open');
 					});
 
 					mval = project["name"].split(' ').join('_').toLowerCase();
 					$location.url('?project=' + mval)
-				 }
+				}
 
-					$scope.search = function (arg) {
-						$scope.searchText = arg
+				$scope.search = function (arg) {
+					$scope.searchText = arg
+				}
+
+				$scope.redirect = function (arg) {
+					window.open(arg, '_blank');
+				}
+
+				$scope.updateLink = function () {
+					$location.url($location.path());
+				}
+
+				$scope.projects_url_dict = {}
+				
+				angular.forEach($scope.projectList, function(value, key){
+					value["url"] = value["name"].split(' ').join('_').toLowerCase();
+					$scope.projects_url_dict[value["url"]] = key
+				});
+
+				var project_requested = $location.search().project; 
+				if(project_requested){
+					if(Object.keys($scope.projects_url_dict).indexOf(project_requested) > -1){
+						self.showProject($scope.projectList[$scope.projects_url_dict[project_requested]])
 					}
+				}
 
-					$scope.redirect = function (arg) {
-						window.open(arg, '_blank');
-					}
-
-					$scope.updateLink = function () {
-						$location.url($location.path());
-					}
-
-	 			$scope.projects_url_dict = {}
- 
- 				angular.forEach($scope.projectList, function(value, key){
- 						value["url"] = value["name"].split(' ').join('_').toLowerCase();
- 						$scope.projects_url_dict[value["url"]] = key
- 				});
-
- 				var project_requested = $location.search().project; 
- 				if(project_requested){
- 					if(Object.keys($scope.projects_url_dict).indexOf(project_requested) > -1){
- 						self.showProject($scope.projectList[$scope.projects_url_dict[project_requested]])
- 						}
- 				}
-
- 				var search_requested = $location.search().q; 
- 				if(search_requested){
- 					$scope.searchText = search_requested
- 				}
+				var search_requested = $location.search().q; 
+				if(search_requested){
+					$scope.searchText = search_requested
+				}
 
 			},
 			controllerAs: 'lc'
@@ -90,20 +90,19 @@
 			controller: function () {
 				self = this
 				self.faqs = faq
-		},
-		controllerAs: 'toc'
-	}
+			},
+			controllerAs: 'toc'
+		}
 	}]);
 
- app.filter('format_issue', function () {
-  return function (value) {
-   if (!value) return '';
-   res = value.split('/')
-   res = res[3] + '/' + res[4] + '#' + res[6]
-   return res;
-  };
- });
-
+	app.filter('format_issue', function () {
+		return function (value) {
+			if (!value) return '';
+			res = value.split('/')
+			res = res[3] + '/' + res[4] + '#' + res[6]
+			return res;
+		};
+	});
 
 	app.directive('mentors', ['$http', function ($http) {
 		return {
@@ -116,19 +115,19 @@
 				angular.forEach(projects, function(value, key){
 					angular.forEach(value.mentors, function(value, key){
 						self.mentorsList[value] =  {
-									"github_handle" : value,
-									"github_avatar_url": "https://avatars.githubusercontent.com/" +value
+							"github_handle" : value,
+							"github_avatar_url": "https://avatars.githubusercontent.com/" +value
 						}
 
 					});
 				});
 
 				angular.forEach(admins, function(value, key){
-						self.adminsList[value] = {
-							"github_handle" : value,
-							"github_avatar_url": "https://avatars.githubusercontent.com/" +value
+					self.adminsList[value] = {
+						"github_handle" : value,
+						"github_avatar_url": "https://avatars.githubusercontent.com/" +value
 
-						}
+					}
 				});
 			},
 			controllerAs: "gic"
